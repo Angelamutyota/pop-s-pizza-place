791f7f7bb48e6f996121c8d1ef7ec6b00b4ee4e7
ANGELA <angela.mbithe@student.moringaschool.com>
2021-04-26 08:47:20 +0300

    hide summary and customer data

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $(function() {
+        $('.summary').hide();
+        $('.cdata-overlay').hide();
+
+        $("#checkout").click(function(){
+            let flavour = $(".flavour option:selected").val();
+            let size = $("#size option:selected").val();
+            let crust = $("#crust option:selected").val();
+            let topping = $("#toppings option:selected").val();
+            let number = $("#number").val();
+            console.log(size);
+
+            //Function order
+             let order = (f, s, c, t, n, total) => {
+            return {f, s, c, t, n, total};
+        };
+        })
+    })
+})
